aa7af5964be9d78ccd701984505bba004f843a80
Abel <abel.rangel@cetys.edu.mx>
2017-09-10 20:21:52 +0200

    Connection of angularfire2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyC7J6DuKSiccFvcjzoIgqYbDmTgq5LQBlA',
+    authDomain: 'zentremedical-app-67145.firebaseapp.com',
+    databaseURL: 'https://zentremedical-app-67145.firebaseio.com',
+    projectId: 'zentremedical-app-67145',
+    storageBucket: 'zentremedical-app-67145.appspot.com',
+    messagingSenderId: '794071201283'
+  }
 };
