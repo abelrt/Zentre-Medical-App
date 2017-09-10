31da4bb8ddacc66e34b06ef91c1dbd588e16652c
Abel <abel.rangel@cetys.edu.mx>
2017-09-10 21:12:44 +0200

    FINAL Basic Stable Version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
 import 'hammerjs';
 
 @Component({
@@ -9,8 +12,21 @@ import 'hammerjs';
 })
 export class AppComponent {
   title = 'app';
+  user: Observable<firebase.User>;
   items: FirebaseListObservable<any[]>;
-  constructor(db: AngularFireDatabase) {
+
+  constructor(public afAuth: AngularFireAuth, db: AngularFireDatabase) {
+    this.user = afAuth.authState;
     this.items = db.list('/items');
+  }
+
+  loginGmail() {
+    this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+  }
+  loginFacebook() {
+    this.afAuth.auth.signInWithPopup(new firebase.auth.FacebookAuthProvider());
+  }
+  logout() {
+    this.afAuth.auth.signOut();
   }
 }
